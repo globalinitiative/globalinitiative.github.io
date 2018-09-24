71b8626462019535e93da4fde5b939b041d77730
Pabloska <pablocadabon@hotmail.com>
2018-09-24 15:03:28 +0200

    Update leaflet_google_sheets.js

--- globalinitiative/js/leaflet_google_sheets.js
+++ globalinitiative/js/leaflet_google_sheets.js
@@ -1,15 +1,17 @@
+
 var map;
+
 var code = "1EXl0ZGn4RMBtim_c4zwA_te3VQgIquVywZlT_XKR0qo"
 
 document.addEventListener('DOMContentLoaded',function(){
   map = L.map('map').setView([22, 11], 4);
-  L.tileLayer('https://{s}.tile.openstreetmap.fr/osmfr/{z}/{x}/{y}.png', {
+  L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/Canvas/World_Dark_Gray_Base/MapServer/tile/{z}/{y}/{x}', {
       attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
   }).addTo(map);
   
    var marker1 = {
     radius: 3,
-    fillColor: "#03A9F4",
+    fillColor: "white",
     // color: "#fff",
     weight: 0,
     opacity: 1,
@@ -17,7 +19,7 @@ document.addEventListener('DOMContentLoaded',function(){
   };
   
   var firefoxIcon = L.icon({
-        iconUrl: 'https://upload.wikimedia.org/wikipedia/commons/thumb/d/de/Google_Map_Marker.svg/2000px-Google_Map_Marker.svg.png',
+        iconUrl: 'https://mbtskoudsalg.com/images/blank-circle-png-3.gif',
         iconSize: [7, 7], // size of the icon
         });
   
@@ -34,5 +36,50 @@ document.addEventListener('DOMContentLoaded',function(){
     },
     simpleSheet: true 
   })
+  
+ 
+ //routes JSON
+ 
+ 
+		function onEachFeature(feature, layer) {
+			var popupContent = "<p><img style='height:50px,' src="+''+" ></img></P>";
+
+			if (feature.properties && feature.properties.popupContent) {
+				popupContent += feature.properties.popupContent;
+			}
+
+			layer.bindPopup(popupContent);
+		}
+
+
+		
+		
+		
+		var myStyle = {
+		 "color": "yellow",
+		 "dashArray": '',
+		 "weight": 1,
+		 "opacity": 0.2
+		};
+
+		
+		
+		
+		L.geoJson(routes, {
+
+			filter: function (feature, layer) {
+				if (feature.properties) {
+					// If the property "underConstruction" exists and is true, return false (don't render features under construction)
+					return feature.properties.underConstruction !== undefined ? !feature.properties.underConstruction : true;
+				}
+				return false;
+			},
+			style: myStyle,
+			onEachFeature: onEachFeature
+		}).addTo(map);
+
+	//labels//
+ 
+ 
   
 })
