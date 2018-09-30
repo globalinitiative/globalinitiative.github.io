f1318f3a1c757923b43cced9f9cb9de4f0d9ce99
globalinitiative <43697566+globalinitiative@users.noreply.github.com>
2018-09-30 16:42:43 +0200

    Update leaflet_google_sheets.js

--- detention-centers/js/leaflet_google_sheets.js
+++ detention-centers/js/leaflet_google_sheets.js
@@ -3,7 +3,7 @@ var code = "1EBf7nu0sh5BNtORew9A0N4MYI12fds0fY17InIlqEGk"
 
 document.addEventListener('DOMContentLoaded',function(){
   map = L.map('map').setView([22, 11], 4);
-  L.tileLayer('https://{s}.tile.openstreetmap.fr/osmfr/{z}/{x}/{y}.png', {
+  L.tileLayer('http://{s}.tiles.wmflabs.org/bw-mapnik/{z}/{x}/{y}.png', {
       attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
   }).addTo(map);
   
