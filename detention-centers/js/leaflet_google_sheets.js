282fdd7614882c35cbf22dac235c35b5473fd2ab
globalinitiative <43697566+globalinitiative@users.noreply.github.com>
2018-09-30 16:50:26 +0200

    Update leaflet_google_sheets.js

--- detention-centers/js/leaflet_google_sheets.js
+++ detention-centers/js/leaflet_google_sheets.js
@@ -2,7 +2,7 @@ var map;
 var code = "1EBf7nu0sh5BNtORew9A0N4MYI12fds0fY17InIlqEGk"
 
 document.addEventListener('DOMContentLoaded',function(){
-  map = L.map('map').setView([22, 11], 4);
+  map = L.map('map').setView([26.822014, 17.182133], 6);
   L.tileLayer('http://{s}.tiles.wmflabs.org/bw-mapnik/{z}/{x}/{y}.png', {
       attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
   }).addTo(map);
@@ -17,8 +17,8 @@ document.addEventListener('DOMContentLoaded',function(){
   };
   
   var firefoxIcon = L.icon({
-        iconUrl: 'https://banner2.kisspng.com/20180605/gtw/kisspng-logo-target-corporation-red-marker-5b16467f1bdee9.6471526315281864951142.jpg',
-        iconSize: [7, 7], // size of the icon
+        iconUrl: 'https://upload.wikimedia.org/wikipedia/commons/thumb/c/c5/Target_Corporation_logo_%28vector%29.svg/1200px-Target_Corporation_logo_%28vector%29.svg.png',
+        iconSize: [12, 12], // size of the icon
         });
   
   
