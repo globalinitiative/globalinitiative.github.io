01a32c46f6e8dea58c1c6937945ed8c9f048a809
globalinitiative <43697566+globalinitiative@users.noreply.github.com>
2018-10-01 11:26:01 +0200

    Update leaflet_google_sheets.js

--- detention-centers/js/leaflet_google_sheets.js
+++ detention-centers/js/leaflet_google_sheets.js
@@ -2,10 +2,46 @@ var map;
 var code = "1EBf7nu0sh5BNtORew9A0N4MYI12fds0fY17InIlqEGk"
 
 document.addEventListener('DOMContentLoaded',function(){
-  map = L.map('map').setView([26.822014, 17.182133], 6);
-  L.tileLayer('http://{s}.tiles.wmflabs.org/bw-mapnik/{z}/{x}/{y}.png', {
+  
+  
+ // map = L.map('map').setView([26.822014, 17.182133], 6);
+  
+  map = L.map('map', {
+		center: [26.822014, 17.182133],
+		zoom: 6,
+		layers: []
+	});
+  
+ L.tileLayer('https://korona.geog.uni-heidelberg.de/tiles/roadsg/x={x}&y={y}&z={z}', {
       attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
   }).addTo(map);
+  
+  // hydrology overlay layer
+  
+  
+var satellite  = L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}.png', {
+      attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+  }),
+
+  arabic  = L.tileLayer('http://{s}.tiles.wmflabs.org/bw-mapnik/{z}/{x}/{y}.png', {
+      attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+  });
+
+
+
+// json object for layer switcher control basemaps
+var baseLayers = {
+		"Satellite": satellite,
+		"Arabic ": arabic 
+	};
+
+/*var overlayMaps = {
+    "Detention Centers": code
+};*/
+
+// add layer groups to layer switcher control
+var controlLayers = L.control.layers(baseLayers).addTo(map);
+  
   
    var marker1 = {
     radius: 3,
@@ -18,7 +54,7 @@ document.addEventListener('DOMContentLoaded',function(){
   
   var firefoxIcon = L.icon({
         iconUrl: 'https://globalinitiative.github.io/detention-centers/img/target_logo_1.png',
-        iconSize: [12, 12], // size of the icon
+        iconSize: [16, 16], // size of the icon
         });
   
   
@@ -29,14 +65,40 @@ document.addEventListener('DOMContentLoaded',function(){
         var place = sheet[i];
         L.marker([place.lat, place.lon], {icon: firefoxIcon})
           .addTo(map)
-          .bindPopup(place.name)
-	      
+          .bindPopup("<strong>Detention center name: </strong>"+ place.name +"<br><strong>Latitude:</strong>"+ place.lat +"<br><strong>Longitude:</strong>"+ place.lon)
+
       }
     },
     simpleSheet: true 
   })
   
-  L.geoJson(routes, {
+ //Libya Boundary JSON
+ 
+ 
+		function onEachFeature(feature, layer) {
+			var popupContent = "<p><img style='height:50px,' src="+''+" ></img></P>";
+
+			if (feature.properties && feature.properties.popupContent) {
+				popupContent += feature.properties.popupContent;
+			}
+
+			layer.bindPopup(popupContent);
+		}
+
+
+		
+		
+		
+		var myStyle = {
+		 "color": 'grey',  //Outline color
+		 "dashArray": '',
+		 
+         
+		 "fillColor": "rgba(38, 12, 12, 0.5)",
+		 "weight": 2,
+		 "opacity": 1
+		}; 
+   L.geoJson(libya, {
 
 			filter: function (feature, layer) {
 				if (feature.properties) {
